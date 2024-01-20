bc1ab59cf03d7f57dab6c92952b72b8c9ae37ff9
Ayman_Rabaya <87930881+AymanRabaya20@users.noreply.github.com>
2024-01-20 12:54:33 +0200

    SuperHeroAPI(.NET)

---
+++ SuperHeroAPI/SuperHeroAPI/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SuperHeroAPI.Data;
+using System.Runtime.CompilerServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(Options => Options.AddPolicy(name: "SuperHeroOrigins",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    }));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("SuperHeroOrigins");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
